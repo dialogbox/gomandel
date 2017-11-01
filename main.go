b3d1d60d93d70f48edb9aff65edda9200e520633
Jason Kim <junchkim@ebay.com>
2017-11-01 17:09:05 +0900

    changed to use channel and limited number of workers

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 type config struct {
 	xmin, ymin, xmax, ymax float64
 	width, height          int
+	workers                int
 }
 
 func (c *config) setPositionStr(posStr string) error {
@@ -51,7 +52,7 @@ func (c *config) setPositionStr(posStr string) error {
 }
 
 func getConfig(r *http.Request) (*config, error) {
-	c := &config{-2, -2, 2, 2, 4096, 4096}
+	c := &config{-2, -2, 2, 2, 4096, 4096, 8}
 
 	posStr := r.FormValue("pos")
 	if posStr != "" {
@@ -78,26 +79,45 @@ func getConfig(r *http.Request) (*config, error) {
 		c.height = int(theight)
 	}
 
+	workersStr := r.FormValue("workers")
+	if workersStr != "" {
+		tworkers, err := strconv.ParseInt(workersStr, 10, 32)
+		if err != nil {
+			return nil, err
+		}
+		c.workers = int(tworkers)
+	}
+
 	return c, nil
 }
 
 func renderMandelbrot(c *config) (image.Image, error) {
 	img := image.NewRGBA(image.Rect(0, 0, c.width, c.height))
 
+	q := make(chan int, 1000)
+
 	var wg sync.WaitGroup
-	for py := 0; py < c.height; py++ {
+	for i := 0; i < c.workers; i++ {
 		wg.Add(1)
-		go func(py int) {
-			for px := 0; px < c.width; px++ {
-				x := float64(px)/float64(c.width)*(c.xmax-c.xmin) + c.xmin
-				y := float64(py)/float64(c.height)*(c.ymax-c.ymin) + c.ymin
-				z := complex(x, y)
-				// Image point (px, py) represents complex value z.
-				img.Set(px, py, mandelbrot(z))
+		go func() {
+			defer wg.Done()
+			for py := range q {
+				for px := 0; px < c.width; px++ {
+					x := float64(px)/float64(c.width)*(c.xmax-c.xmin) + c.xmin
+					y := float64(py)/float64(c.height)*(c.ymax-c.ymin) + c.ymin
+					z := complex(x, y)
+					// Image point (px, py) represents complex value z.
+					img.Set(px, py, mandelbrot(z))
+				}
 			}
-			wg.Done()
-		}(py)
+		}()
 	}
+
+	for py := 0; py < c.height; py++ {
+		q <- py
+	}
+	close(q)
+
 	wg.Wait()
 
 	return img, nil
@@ -113,12 +133,21 @@ func mandelHandler(w http.ResponseWriter, r *http.Request) {
 
 	ts := time.Now()
 
+	// traceFile, err := os.Create("mandel.trace")
+	// if err != nil {
+	// 	log.Println(err)
+	// } else {
+	// 	trace.Start(traceFile)
+	// }
+
 	img, err := renderMandelbrot(c)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(500)
 		return
 	}
+
+	// trace.Stop()
 
 	fmt.Println(time.Now().Sub(ts))
 
